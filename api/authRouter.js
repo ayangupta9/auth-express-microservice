e92cc9b32ca70f15c92faeb23e4f42126e7c0392
Ayan Gupta <ayangupta1000@gmail.com>
2022-07-05 03:53:46 +0530

    init deploy

--- api/authRouter.js
+++ api/authRouter.js
@@ -64,8 +64,8 @@ router.post('/login', async (req, res) => {
         httpOnly: true,
         // maxAge: 1000 * 60 * 60 * 10
         maxAge: 18000000,
-        sameSite: 'none',
-        secure: true,
+        // sameSite: 'none',
+        // secure: true,
         domain: 'vercel.app'
       })
       .json({
